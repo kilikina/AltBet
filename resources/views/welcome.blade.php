7577c8f2798af821f26411553d9539cbd0fb5b3e
Kilikina <kilikina@gmx.com>
2015-08-02 14:29:18 -0700

    Update landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,58 +6,21 @@
         <link href="//fonts.googleapis.com/css?family=Lato:100,300" rel="stylesheet" type="text/css">
 
         <style>
-            html {
-                min-height: 100%;
-            }
-
-            body {
-                margin: 0 0 100px;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            footer {
-                position: absolute;
-                bottom: 0;
-                height: 100px;
-                width: 100%;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-                font-weight: 300;
-            }
-
-            .title-caption {
-                font-size: 32px;
-                font-style: italic;
-            }
+            
 
 
         </style>
     </head>
     <body>
-        <div class="container">
+        <div class="wrapper">
             <nav>
                 
             </nav>
-            <div class="content">
-                <div class="title">AltBit</div>
-                <div class="title-caption">Feeling lucky?</div>
+            <div class="vertical-center">
+                <div class="center">
+                    <div class="title">AltBit</div>
+                    <div class="title-caption">Feeling lucky?</div>
+                </div>
             </div>
             <footer class="">
                 <div class="caption">Coming Soon to AltBit.io</div>
