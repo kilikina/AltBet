fb701296509ffed4491c00c613484883877033f5
Kilikina <kilikina@gmx.com>
2015-08-02 16:29:06 -0700

    Modify landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,19 +4,65 @@
         <title>AltBit</title>
 
         <link href="//fonts.googleapis.com/css?family=Lato:100,300" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" type="text/css" href="/css/app.css">
-        
+
+        <style>
+            html {
+                min-height: 100%;
+            }
+
+            body {
+                margin: 0 0 100px;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            footer {
+                position: absolute;
+                bottom: 0;
+                height: 100px;
+                width: 100%;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+                width: 0;
+                height: 100%;
+                min-height: inherit;
+                max-height: inherit;
+                padding: 0;
+            }
+
+            .title {
+                font-size: 96px;
+                font-weight: 300;
+            }
+
+            .title-caption {
+                font-size: 32px;
+                font-style: italic;
+            }
+
+
+        </style>
     </head>
     <body>
-        <div class="wrapper">
+        <div class="container">
             <nav>
                 
             </nav>
-            <div class="vertical-center">
-                <div class="center">
-                    <div class="title">AltBit</div>
-                    <div class="title-caption">Feeling lucky?</div>
-                </div>
+            <div class="content">
+                <div class="title">AltBit</div>
+                <div class="title-caption">Feeling lucky?</div>
             </div>
             <footer class="">
                 <div class="caption">Coming Soon to AltBit.io</div>
