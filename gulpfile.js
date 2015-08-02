35dbedc0c91ea84fa860718baa722869efeba829
Kilikina <kilikina@gmx.com>
2015-08-02 16:29:40 -0700

    Define gulp task to merge app with bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
 
+/*
+ | Copy files.
+ | 'gulp copyf' after bower updates
+ |
+ */
+gulp.task('copyf', function() {
+	gulp.src('vendor/bower_components/bootstrap/less/**')
+		.pipe(gulp.dest('resources/assets/less/bootstrap'));
+	gulp.src('vendor/bower_components/bootstrap.dist.js/bootstrap.js')
+		.pipe(gulp.dest('resources/assets/js/'));
+	gulp.src('vendor/bower_components/bootstrap/dist/fonts/**')
+		.pipe(gulp.dest('public/assets/fonts'));
+	gulp.src('vendor/bower_components/jquery/dist/jquery.js')
+		 .pipe(gulp.dest('resources/assets/js/'));
+});
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -12,6 +28,16 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
+	// Compile Less
     mix.less('app.less').coffee;
+
+    // Combine scripts
+    mix.scripts([
+    	'js/jquery.js',
+    	'js/bootstrap.js'
+    ],
+    'public/assets/js/admin.js',
+    'resources/assets'
+    );
     // mix.browserfy('index.js');
 });
