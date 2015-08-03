b8a04f1da289025081286516128c3fa2029d99ab
Kilikina <kilikina@gmx.com>
2015-08-02 17:05:03 -0700

    Update layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,65 +1,40 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <title>AltBit</title>
 
         <link href="//fonts.googleapis.com/css?family=Lato:100,300" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="/assets/css/app.css">
+        @yield('styles')
 
-        <style>
-            html {
-                min-height: 100%;
-            }
-
-            body {
-                margin: 0 0 100px;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            footer {
-                position: absolute;
-                bottom: 0;
-                height: 100px;
-                width: 100%;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-                width: 0;
-                height: 100%;
-                min-height: inherit;
-                max-height: inherit;
-                padding: 0;
-            }
-
-            .title {
-                font-size: 96px;
-                font-weight: 300;
-            }
-
-            .title-caption {
-                font-size: 32px;
-                font-style: italic;
-            }
-
-
-        </style>
+        <!--[if lt IE 9]>
+            <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-toggle="#navbar-menu">
+                        <span class="sr-only">Toggle Nav</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">{{ config('app.title') }} Admin</a>
+                </div>
+                <div class="collapse navbar-collapse" id="navbar-menu">
+                    @include('admin.partials.navbar')
+                </div>
+            </div>
+        </nav>
+        @yield('content')
         <div class="container">
-            <nav>
-                
-            </nav>
             <div class="content">
                 <div class="title">AltBit</div>
                 <div class="title-caption">Feeling lucky?</div>
@@ -68,5 +43,6 @@
                 <div class="caption">Coming Soon to AltBit.io</div>
             </footer>
         </div>
+        <script src="{{ asset('/js/app.js') }}"></script>
     </body>
 </html>
